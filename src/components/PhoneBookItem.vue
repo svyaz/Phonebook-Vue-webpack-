bb055ea3fbb3382687affa6f3e71fb27ebfcaa25
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-08-24 23:17:19 +0700

    Fix. Пробелы вокруг +

--- src/components/PhoneBookItem.vue
+++ src/components/PhoneBookItem.vue
@@ -3,7 +3,7 @@
         <div class="col-1"><label><input type="checkbox"
                                          :value="item.id"
                                          v-model="selected"></label></div>
-        <div class="col-1" v-text="index+1"></div>
+        <div class="col-1" v-text="index + 1"></div>
         <div class="col-5" v-text="item.fullName"></div>
         <div class="col-4" v-text="item.phoneNumber"></div>
         <div class="col-1">
